217418f05b432c0090dd78a14f17111029380b6a
Корнилов Олег <okornilov@diasoft.ru>
2020-06-20 14:36:39 +0300

    Реализация ДЗ "Продвинутая конфигурация Spring-приложений

--- home-work-04/src/main/java/ru/otus/questionnaire/config/AppConfig.java
+++ home-work-04/src/main/java/ru/otus/questionnaire/config/AppConfig.java
@@ -1,10 +1,8 @@
 package ru.otus.questionnaire.config;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import ru.otus.questionnaire.service.IOService;
 import ru.otus.questionnaire.service.QuestionIOService;
 
@@ -16,12 +14,10 @@ import java.util.Scanner;
 public class AppConfig {
 
     @Bean
-    @Profile("!test")
     public IOService ioService() {
         return new QuestionIOService(new PrintStream(System.out), new Scanner(System.in));
     }
 
-    @ConfigurationProperties(prefix = "questions")
     @Bean
     public QuestionProps questionProps() {
         return new QuestionProps();
--- home-work-04/src/main/java/ru/otus/questionnaire/config/QuestionProps.java
+++ home-work-04/src/main/java/ru/otus/questionnaire/config/QuestionProps.java
@@ -2,9 +2,11 @@ package ru.otus.questionnaire.config;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @Getter
 @Setter
+@ConfigurationProperties(prefix = "questions")
 public class QuestionProps {
     private String fileName;
     private Float requiredCorrectAnswerPercent;
